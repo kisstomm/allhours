3805f04dfe92897dc2f28b609557491b56581b54
kisstomm <kisstomm@gmail.com>
2024-08-23 16:59:35 +0200

    add error handling, when fields are empty

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {Router} from "@angular/router";
 import {MessageService} from "primeng/api";
-import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-settings',
@@ -11,24 +10,56 @@ import { ToastModule } from 'primeng/toast';
 export class SettingsComponent {
   clientId: string;
   clientSecret: string;
+  isShowError: Map<string, boolean>;
 
   constructor(private router: Router, private messageService: MessageService) {
     this.clientId = localStorage.getItem('clientId') || "";
     this.clientSecret = localStorage.getItem('clientSecret') || "";
+    this.isShowError = new Map<string, boolean>;
+  }
+
+  onClientIdChange() {
+    const isError: boolean = this.clientId === "" || this.clientId === null;
+    this.isShowError.set('clientId', isError);
+
+  }
+
+  onClientSecretChange() {
+    const isError: boolean = this.clientSecret === "" || this.clientSecret === null;
+    this.isShowError.set('clientSecret', isError);
   }
 
   onSave() {
-    try {
-      this.saveDataToLocalStorage();
-      this.messageService.add({ severity: 'success', summary: 'Settings saved', detail: '' });
-    } catch (e) {
-      this.messageService.add({ severity: 'error', summary: 'Error during save settings', detail: '' });
+    this.validateFields();
+    if (this.isSaveButtonEnabled()) {
+      try {
+        this.saveDataToLocalStorage();
+        this.messageService.add({severity: 'success', summary: 'Settings saved', detail: ''});
+        this.router.navigate(['/']);
+      } catch (e) {
+        this.messageService.add({severity: 'error', summary: 'Error during save settings', detail: ''});
+      }
     }
   }
 
   onCancel() {
-    this.router.navigate(['/']);
     this.messageService.add({ severity: 'info', summary: 'Settings restored', detail: '' });
+    this.router.navigate(['/']);
+  }
+
+  isSaveButtonEnabled(): boolean {
+    for (let [key, isError] of this.isShowError) {
+      if (isError) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  private validateFields() {
+    this.onClientIdChange();
+    this.onClientSecretChange();
   }
 
   private saveDataToLocalStorage() {
