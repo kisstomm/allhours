ba1e8147ef77ba971ad9077322b07da6e4c1b5f5
kisstomm <kisstomm@gmail.com>
2024-08-25 15:46:34 +0200

    load absences

--- src/app/components/absence-list/absence-list.component.ts
+++ src/app/components/absence-list/absence-list.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {UserDto} from "../../dto/UserDto";
+import {AbsenceDto} from "../../dto/AbsenceDto";
+import {AbsenceService} from "../../service/absence.service";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-absence-list',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./absence-list.component.scss']
 })
 export class AbsenceListComponent {
+  items: AbsenceDto[];
+  isLoading: boolean;
 
+  constructor(private absenceService: AbsenceService, private messageService: MessageService) {
+    this.items = [];
+    this.isLoading = false;
+
+    this.absenceService.getAll().subscribe({
+      next: data => {
+        this.items = data;
+        this.isLoading = false;
+      },
+      error: err => {
+        let detail: string = '';
+        if(err.status === 401) {
+          detail = 'Authentication needed';
+        }
+        this.messageService.add({severity: 'error', summary: 'Error during load absences', detail});
+        this.isLoading = false;
+      }
+    })
+  }
 }
